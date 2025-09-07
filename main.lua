36ed060bd995b5dd573e9bf513194331d20f4aab
totallyeyahyeahyeah <a4828525@gmail.com>
2025-09-07 16:28:07 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -252,7 +252,7 @@ title_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
 title_5.BorderSizePixel = 0
 title_5.Size = UDim2.new(0, 449, 0, 20)
 title_5.Font = Enum.Font.Arial
-title_5.Text = "Байцовске Мафия 69 / t.me/baicovsky_mafia69"
+title_5.Text = "Baicovske Mafia 69 / t.me/baicovsky_mafia69"
 title_5.TextColor3 = Color3.fromRGB(255, 255, 255)
 title_5.TextSize = 12.000
 
@@ -512,7 +512,7 @@ fake.BorderSizePixel = 0
 fake.Position = UDim2.new(0.0112812826, 0, 0.441860467, 0)
 fake.Size = UDim2.new(0, 208, 0, 47)
 fake.Font = Enum.Font.SourceSans
-fake.Text = " Байцовске мафия 69 GUI V1 - 1.69 \n yeah"
+fake.Text = " Baicovske Mafia 69 GUI V1 - 1.69 \n yeah"
 fake.TextColor3 = Color3.fromRGB(255, 255, 255)
 fake.TextSize = 14.000
 fake.TextXAlignment = Enum.TextXAlignment.Left
